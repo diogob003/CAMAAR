e5ef09a9aeb6b400a9f7a41bf71f6d5c13d2a18f
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2025-07-18 19:17:30 -0300

    Remove duplicates of code to handle sidebar and logout; created app.js

--- src/app/javascript/admin_templates.new.js
+++ src/app/javascript/admin_templates.new.js
@@ -1,26 +1,3 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('mySidebar');
-    sidebar.classList.toggle('open');
-}
-
-function toggleLogoutDropdown() {
-    const dropdown = document.getElementById('logoutDropdown');
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.user-icon') && !event.target.matches('.user-icon *')) {
-        const dropdowns = document.getElementsByClassName("logout-dropdown");
-        for (let i = 0; i < dropdowns.length; i++) {
-            const openDropdown = dropdowns[i];
-            if (openDropdown.style.display === 'block') {
-                openDropdown.style.display = 'none';
-            }
-        }
-    }
-}
-
 function addListeners() {
     const addBtn = document.getElementById("add-question-btn");
     const questionsList = document.getElementById("questions-list");
